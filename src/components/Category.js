398158f109fa5603fd08c3235147b1eecc56f68a
Raisul islam piaus <54791396+raisulislampiaus@users.noreply.github.com>
2023-07-30 23:49:47 +0600

    Update Category.js

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,7 +4,6 @@ function Category({ category }) {
     return (
         <div className='categorys'>
             <div className='ctgItem'>
-                <img className='imageItems' src={category.image} alt="" />
                 <h6 className='ctgName'>{category.name}</h6>
             </div>
         </div>
